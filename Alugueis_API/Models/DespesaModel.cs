df7a537686e3ff606777aee7468b7aa96b693e8a
Natan Valente <natan0404@hotmail.com>
2025-10-27 20:55:58 -0300

    feat: Adicionado campo no despesaModel

--- Alugueis_API/Models/DespesaModel.cs
+++ Alugueis_API/Models/DespesaModel.cs
@@ -7,6 +7,7 @@
         public TipoDespesa TipoDespesa { get; set; }
         public float VrlTotalDespesa { get; set; }
         public DateTime DataDespesa { get; set; }
+        public DateTime CompetenciaMes { get; set; }
         public ICollection<DespesaRateio> Rateios { get; set; }
     }
 }
